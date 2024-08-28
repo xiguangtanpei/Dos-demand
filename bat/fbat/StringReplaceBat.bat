4f848dcfaa9020dc43da5a35b2823bdf1ab4547a
tanpei <tanpei@tencent.com>
2024-08-28 14:50:51 +0800

    加入第一个真实函 进行字符串替换工作

---
+++ bat/fbat/StringReplaceBat.bat
@@ -0,0 +1,11 @@
+@echo  off 
+chcp 65001>nul 
+rem  字符串替换函数  传入一个字符串 替换另一个  
+setlocal  
+set  "GetString=%1"
+set  "OldToken=%2"
+set  "NewToken=%3"
+
+call  set  "result=%%GetString:%OldToken%=%NewToken%%%"
+rem echo  %result%
+endlocal & set "GLOBAL_VALUE_StringReplaceBat=%result%"
---
+++ bat/run.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001>nul
+echo "运行 run.bat"
+setlocal 
+
+
+@REM rem 测试字符串替换工作  
+@REM set "test1=E:\D\github\dos_demand\bat"
+@REM call  fbat/StringReplaceBat.bat  %test1%  github  ch 
+@REM echo  %GLOBAL_VALUE_StringReplaceBat% 
+@REM rem 返回  E:\D\ch\dos_demand\bat  正确 
+
+
+
+
+
+endlocal 
+pause>nul 
